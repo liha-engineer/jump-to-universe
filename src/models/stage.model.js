96679878786b0531f92b5f61a37a32f3f814dc1a
liha-engineer <liha.engineer@gmail.com>
2024-10-04 12:35:35 +0900

    feat: clearStage 추가

--- src/models/stage.model.js
+++ src/models/stage.model.js
@@ -1,9 +1,10 @@
 
 // key : uuid, value : array -> stage정보는 배열로 들어올거임 (여러개라서)
+// 이제 우리가 받아올 스테이지는 public -> assets에 있는 스테이지를 받아오면 되는데...
 const stages = {};
 
 // 스테이지 초기화
-const createStage = (uuid) => {
+export const createStage = (uuid) => {
     stages[uuid] = [];
 }
 
@@ -13,4 +14,8 @@ export const getStage = (uuid) => {
 
 export const setStage = (uuid, id, timestamp) => {
     return stages[uuid].push({ id, timestamp })
+}
+
+export const clearStage = (uuid) => {
+    stages[uuid] = [];
 }
