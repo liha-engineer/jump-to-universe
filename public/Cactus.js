6a6d78ef3dc2654041271306ee47a1cbed8e507f
liha-engineer <liha.engineer@gmail.com>
2024-10-04 12:38:56 +0900

    feat: 장애물 y좌표 난수화

--- public/Cactus.js
+++ public/Cactus.js
@@ -2,7 +2,7 @@ class Cactus {
   constructor(ctx, x, y, width, height, image) {
     this.ctx = ctx;
     this.x = x;
-    this.y = y;
+    this.y = y * Math.random();
     this.width = width;
     this.height = height;
     this.image = image;
