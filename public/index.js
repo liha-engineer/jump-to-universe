d75112c6be95dd12a76d476f80bd5d7cf42fa967
liha-engineer <liha.engineer@gmail.com>
2024-10-05 19:38:29 +0900

    feat: 오디오 파일 추가 및 반영

--- public/index.js
+++ public/index.js
@@ -1,8 +1,10 @@
 import Player from './Player.js';
-import Ground from './Ground.js';
+import Track from './Track.js';
 import CactiController from './CactiController.js';
 import Score from './Score.js';
 import ItemController from './ItemController.js';
+import './Socket.js';
+import { sendEvent } from './Socket.js';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
@@ -12,7 +14,7 @@ const GAME_SPEED_INCREMENT = 0.00001;
 
 // 게임 크기
 const GAME_WIDTH = 800;
-const GAME_HEIGHT = 200;
+const GAME_HEIGHT = 300;
 
 // 플레이어
 // 800 * 200 사이즈의 캔버스에서는 이미지의 기본크기가 크기때문에 1.5로 나눈 값을 사용. (비율 유지)
@@ -29,8 +31,8 @@ const GROUND_SPEED = 0.5;
 // 선인장
 const CACTI_CONFIG = [
   { width: 48 / 1.5, height: 100 / 1.5, image: 'images/rtan_obstacle_red.png' },
-  { width: 98 / 1.5, height: 100 / 1.5, image: 'images/rtan_obstacle_navy.png' },
-  { width: 68 / 1.5, height: 70 / 1.5, image: 'images/rtan_obstacle_grey.png' },
+  { width: 88 / 1.5, height: 100 / 1.5, image: 'images/rtan_obstacle_red.png' },
+  { width: 68 / 1.5, height: 70 / 1.5, image: 'images/rtan_obstacle_red.png' },
   { width: 68 / 1.5 , height: 70 / 1.5, image: 'images/rtan_obstacle_red.png' },
 ];
 
@@ -54,6 +56,7 @@ let ground = null;
 let cactiController = null;
 let itemController = null;
 let score = null;
+let track = null;
 
 let scaleRatio = null;
 let previousTime = null;
@@ -61,6 +64,19 @@ let gameSpeed = GAME_SPEED_START;
 let gameover = false;
 let hasAddedEventListenersForRestart = false;
 let waitingToStart = true;
+
+const rtanCrash = new Image();
+rtanCrash.src = "./images/rtan_crash.png"
+
+// 오디오 넣어보기
+const bgmSound = new Audio();
+bgmSound.src = "./sounds/bgm.mp3";
+const jumpSound = new Audio();
+jumpSound.src = './sounds/jump.mp3';
+const scoreSound = new Audio();
+scoreSound.src = "./sounds/score.mp3";
+const defeatSound = new Audio();
+defeatSound.src = "./sounds/defeat1.mp3";
 
 function createSprites() {
   // 비율에 맞는 크기
@@ -83,7 +99,8 @@ function createSprites() {
     scaleRatio,
   );
 
-  ground = new Ground(ctx, groundWidthInGame, groundHeightInGame, GROUND_SPEED, scaleRatio);
+  track = new Track(ctx, 800 * scaleRatio, 500 * scaleRatio * 0.6, 0, scaleRatio);
+  // ground = new Ground(ctx, groundWidthInGame, groundHeightInGame, GROUND_SPEED, scaleRatio);
 
   const cactiImages = CACTI_CONFIG.map((cactus) => {
     const image = new Image();
@@ -140,21 +157,21 @@ if (screen.orientation) {
 }
 
 function showGameOver() {
-  const fontSize = 70 * scaleRatio;
-  ctx.font = `${fontSize}px Verdana`;
-  ctx.fillStyle = 'grey';
-  const x = canvas.width / 4.5;
-  const y = canvas.height / 2;
-  ctx.fillText('GAME OVER', x, y);
+  const fontSize = 40 * scaleRatio;
+  ctx.font = `italic bold ${fontSize}px Arial`;
+  ctx.fillStyle = 'salmon';
+  const x = canvas.width / 5;
+  const y = canvas.height / 2.1;
+  ctx.fillText('404 ruined your meat! :(', x, y);
 }
 
 function showStartGameText() {
   const fontSize = 40 * scaleRatio;
-  ctx.font = `${fontSize}px Verdana`;
-  ctx.fillStyle = 'grey';
-  const x = canvas.width / 14;
-  const y = canvas.height / 2;
-  ctx.fillText('Tap Screen or Press Space To Start', x, y);
+  ctx.font = `${fontSize}px Courier New`;
+  ctx.fillStyle = 'white';
+  const x = canvas.width / 8;
+  const y = canvas.height / 2.1;
+  ctx.fillText('Press space for get meat!', x, y);
 }
 
 function updateGameSpeed(deltaTime) {
@@ -166,13 +183,15 @@ function reset() {
   gameover = false;
   waitingToStart = false;
 
-  ground.reset();
+  track.reset();
   cactiController.reset();
   score.reset();
   gameSpeed = GAME_SPEED_START;
+  sendEvent(2, { timestamp : Date.now()})
 }
 
 function setupGameReset() {
+  bgmSound.pause();
   if (!hasAddedEventListenersForRestart) {
     hasAddedEventListenersForRestart = true;
 
@@ -202,9 +221,10 @@ function gameLoop(currentTime) {
   clearScreen();
 
   if (!gameover && !waitingToStart) {
+    bgmSound.volume = 0.2;
+    bgmSound.play();
     // update
-    // 땅이 움직임
-    ground.update(gameSpeed, deltaTime);
+    track.update(gameSpeed, deltaTime)
     // 선인장
     cactiController.update(gameSpeed, deltaTime);
     itemController.update(gameSpeed, deltaTime);
@@ -217,26 +237,38 @@ function gameLoop(currentTime) {
 
   if (!gameover && cactiController.collideWith(player)) {
     gameover = true;
+    defeatSound.volume = 0.2;
+    defeatSound.play();
     score.setHighScore();
     setupGameReset();
   }
   const collideWithItem = itemController.collideWith(player);
   if (collideWithItem && collideWithItem.itemId) {
     score.getItem(collideWithItem.itemId);
+    scoreSound.play();
+    setTimeout(() => {
+      scoreSound.pause(); // 일정 시간 후에 오디오 일시정지
+      scoreSound.currentTime = 0; // 오디오 재생 위치를 시작으로 재설정
+    }, 350); 
+    
   }
 
   // draw
+  track.draw();
   player.draw();
   cactiController.draw();
-  ground.draw();
+  // ground.draw();
   score.draw();
   itemController.draw();
+ 
 
   if (gameover) {
+    bgmSound.pause();
     showGameOver();
   }
 
   if (waitingToStart) {
+    bgmSound.pause();
     showStartGameText();
   }
 
