b6d813a81888df25ba1dd6bfd39465079793eccc
liha-engineer <liha.engineer@gmail.com>
2024-10-05 19:37:57 +0900

    feat: Ground 대신 Track 파일 추가

---
+++ public/Track.js
@@ -0,0 +1,44 @@
+class Wall {
+
+  wallImages = [];
+
+  constructor(ctx, width, height, speed, scaleRatio) {
+    this.ctx = ctx;
+    this.canvas = ctx.canvas;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.scaleRatio = scaleRatio;
+
+    this.x = 0;
+    this.y = 0;
+    // this.y = this.canvas.height;
+
+    this.track1 = new Image();
+    this.track1.src = 'images/background_track.png';
+
+    this.track2 = new Image();
+    this.track2.src = 'images/background_track2.png';
+  }
+
+  update(gameSpeed, deltaTime) {
+    this.x -= gameSpeed * deltaTime * this.speed * this.scaleRatio;
+}
+
+  draw() {
+    // 배경 두개 이어붙이기
+    this.ctx.drawImage(this.track1, this.x, this.y, this.width, this.height);
+    this.ctx.drawImage(this.track2, this.x + this.width, this.y, this.width, this.height);
+
+    // 땅이 끝났을 때 처음으로
+    if (this.x < -this.width) {
+      this.x = 0;
+    }
+  }
+
+  reset() {
+    this.x = 0;
+  }
+}
+
+export default Wall;
