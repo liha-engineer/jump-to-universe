0ef4eeaf7a412be386a9ff44900c7485c7af3fb6
liha-engineer <liha.engineer@gmail.com>
2024-10-12 01:29:30 +0900

    feat: createStage, clearStage를 initializaStage로 통합

--- src/models/stage.model.js
+++ src/models/stage.model.js
@@ -1,21 +1,16 @@
-
 // key : uuid, value : array -> stage정보는 배열로 들어올거임 (여러개라서)
 // 이제 우리가 받아올 스테이지는 public -> assets에 있는 스테이지를 받아오면 되는데...
 const stages = {};
 
 // 스테이지 초기화
-export const createStage = (uuid) => {
-    stages[uuid] = [];
-}
+export const initializeStage = (uuid) => {
+  stages[uuid] = [];
+};
 
 export const getStage = (uuid) => {
-    return stages[uuid];
-}
+  return stages[uuid];
+};
 
 export const setStage = (uuid, id, timestamp) => {
-    return stages[uuid].push({ id, timestamp })
-}
-
-export const clearStage = (uuid) => {
-    return stages[uuid] = [];
-}
+  return stages[uuid].push({ id, timestamp });
+};
