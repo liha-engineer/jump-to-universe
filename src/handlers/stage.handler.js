c7df57105feb04a288a963aacb745adef4098bbd
liha-engineer <liha.engineer@gmail.com>
2024-10-12 01:50:33 +0900

    comment: 불필요한 주석 정리

--- src/handlers/stage.handler.js
+++ src/handlers/stage.handler.js
@@ -6,37 +6,30 @@ import { getGameAssets } from '../init/asset.js';
 import { calculateTotalScore } from '../utils/calculateTotalScore.js';
 
 export const moveStageHandler = (userId, payload) => {
-  // 클라이언트가 currnetStage, 다음에 넘어갈 targetStage 정보 전해줄 것 - 검증 필요
 
-  // 우선 유저의 현재 스테이지 정보 불러오고, 최대 스테이지 ID 찾는다
   let currentStages = getStage(userId);
   if (!currentStages.length) {
     return { status: 'fail', message: 'No stages found for this user' };
   }
 
-  // 유저가 지나간 스테이지는 여러개일 수 있어서, currnetStages 오름차순 정렬하고 가장 끝 스테이지를 가져온다 (이게 현재 스테이지일 것)
   currentStages.sort((a, b) => a.id - b.id);
-  const currentStage = currentStages[currentStages.length - 1];
+  const currentStage = currentStages.at(-1);
 
   // 클라이언트에서 보내주는 스테이지 정보가 서버에서 구한 값과 같은지 검증 - 여기서 쓰려고 payload 받는 것
   if (currentStage.id !== payload.currentStage) {
     return { status: 'fail', message: 'Current stage does not match' };
   }
 
-  // 넘어갈 targetStage가 유효한지 검증
   const { stages } = getGameAssets();
   if (!stages.data.some((stage) => stage.id === payload.targetStage)) {
     return { status: 'fail', message: 'Target stage does not match' };
   }
-
-  // 유저의 현재 점수와 DB에 저장된 점수를 비교해서, 유저의 점수가 더 높으면 다음 스테이지로 올려준다 -> 이건 과제로
 
   const targetStageInfo = stages.data.find((stage) => stage.id === payload.targetStage);
   if (!targetStageInfo) {
     return { status: 'fail', message: 'Target stage not found' };
   }
 
-  // 점수 검증
   const serverTime = Date.now(); // 현재 타임스탬프를 구함
   const userItems = getuserItems(userId);
   const totalScore = calculateTotalScore(currentStages, serverTime, true, userItems);
