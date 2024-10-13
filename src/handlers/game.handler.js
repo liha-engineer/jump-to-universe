96538e6f299dfe30837fd1ba3de0edfd7963e053
liha-engineer <liha.engineer@gmail.com>
2024-10-13 23:49:17 +0900

    fix: 게임 시작 시 얻은 아이템 목록 초기화하여 토탈스코어 값 비정상 현상 수정

--- src/handlers/game.handler.js
+++ src/handlers/game.handler.js
@@ -1,21 +1,22 @@
 import { getGameAssets } from '../init/asset.js';
 import { setStage, getStage, initializeStage } from '../models/stage.model.js';
 import { calculateTotalScore } from '../utils/calculateTotalScore.js';
-import { getuserItems } from '../models/item.model.js';
+import { getuserItems, initializeItems } from '../models/item.model.js';
 
 export const gameStart = (uuid, payload) => {
   const { stages } = getGameAssets();
   initializeStage(uuid);
-  // stages 배열의 0번째 = 첫번째 스테이지 & 클라이언트에서 현재 시작하는 시간을 받아 서버에 저장할 것
+  initializeItems(uuid);
+  // 첫번째 스테이지 ID & 클라이언트에서 현재 시작하는 시간을 받아 서버에 저장할 것
   setStage(uuid, stages.data[0].id, payload.timestamp);
   console.log('Stage: ', getStage(uuid));
 
-  return { status: 'success ' };
+  return { status: 'success' };
 };
 
 export const gameEnd = (uuid, payload) => {
   // 클라 -> 서버로 게임종료시점과 점수를 전달할 것
-  const { timestamp: gameEndTime, score } = payload;
+  const { timestamp: gameEndTime, score, highScore } = payload;
   const stages = getStage(uuid);
   const userItems = getuserItems(uuid);
 
@@ -25,8 +26,8 @@ export const gameEnd = (uuid, payload) => {
 
   const totalScore = calculateTotalScore(stages, gameEndTime, false, userItems);
 
-  // 점수와 타임스탬프 검증 - 오차범위는 5 정도
-  if (Math.abs(score - totalScore) > 5) {
+  // 점수와 타임스탬프 검증 -> Score.js에서 deltaTime * 0.01(10배) 하여 여기도 10 정도 설정
+  if (Math.abs(score - totalScore) > 10) {
     return { status: 'fail', message: 'Score verification failed' };
   }
 
@@ -36,5 +37,5 @@ export const gameEnd = (uuid, payload) => {
   // DB에 저장한다고 가정하면 아래와 같이 쓸 수 있을 것
   // setResult(userId, score, timestamp)
 
-  return { status: 'success ', message: 'Game ended' };
+  return { status: 'success', message: 'Game ended', handler: 3 };
 };
